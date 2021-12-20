0ea8b0b204e472adf0d92bdd496d600e489fe57f
llolo121221 <salvarezb1212@gmail.com>
2021-12-20 10:39:46 -0500

    SE AGREGA PERIOD

---
+++ ClasesCore_Modulo1/src/com/jseac/clasescore/clasesstring/java8Date/Periodo.java
@@ -0,0 +1,16 @@
+package com.jseac.clasescore.clasesstring.java8Date;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Periodo {
+
+	public static void main(String[] args) {
+		
+		Period periodo = Period.between(LocalDate.now(), LocalDate.now().plusDays(10));
+		System.out.println(periodo.getDays());
+		System.out.println(periodo.getMonths());
+		
+	}
+	
+}
